07da92f3bd5d4dd29b330f90624d1e5372018e0d
Lee-sunghawn99 <89784307+Lee-sunghawn99@users.noreply.github.com>
2022-08-22 20:32:53 +0900

    Update count_classes.py

--- TestCode/count_classes.py
+++ TestCode/count_classes.py
@@ -1,16 +1,19 @@
 import rospy
-from detection_msgs.msg import BoundingBox
+from detection_msgs.msg import BoundingBoxes
+
 
 def callback(dat):
-	cnt = 0
-	for each in data.bounding_boxes:
-		if each.Class == "normal fruit":
-			cnt=cnt+1
-	print "normal count :" + str(cnt)
+    cnt = 0
+    for each in data.bounding_boxes:
+        if each.Class == "normal fruit":
+            cnt = cnt + 1
+    print("normal count :" + str(cnt))
 
-rospy.init_node("count_classes",anonymous=False)
-rospy.loginfo("--> Start count)
 
-rospy.Subscriber("detectedd_object",BoundingBox,callback)
+rospy.init_node("/yolov5/detections", anonymous=False)
+rospy.loginfo("--> Start count")
 
-rospy.spin() 
+rospy.Subscriber("detected_object", BoundingBoxes, callback)
+
+rospy.spin()
+
