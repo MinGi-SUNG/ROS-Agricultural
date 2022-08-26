eecf0477f0a31f15cb3a7da44f96950801d329d7
Haneugyu <105436435+Haneugyu@users.noreply.github.com>
2022-08-27 02:39:38 +0900

    Update Sum_left_and_right_fruits.py

--- TestCode/Sum_left_and_right_fruits.py
+++ TestCode/Sum_left_and_right_fruits.py
@@ -3,18 +3,28 @@ for i in range(12):
     left_fruit = [[random.randrange(1,10)]*2 for i in range(12)]
     right_fruit = [[random.randrange(1,10)]*2 for i in range(12)]
 
+# print(left_fruit)
+# print(right_fruit)
+
 for i in range(4):
-    i=7-i
-    left_fruit[i][0] = left_fruit[i][0] + left_fruit[8][0]
-    left_fruit[i][1] = left_fruit[i][1] + left_fruit[8][1]
-    del left_fruit[8]
+    i=4+i
+    left_fruit[i][0] = left_fruit[i][0] + left_fruit[15-i][0]
+    left_fruit[i][1] = left_fruit[i][1] + left_fruit[15-i][1]
+    del left_fruit[15-i]
+left_fruit[4:8] = left_fruit[7], left_fruit[6], left_fruit[5], left_fruit[4]
+
+# print(left_fruit)
 
 for i in range(4):
     right_fruit[i][0] = right_fruit[i][0] + right_fruit[7-i][0]
     right_fruit[i][1] = right_fruit[i][1] + right_fruit[7-i][1]
     del right_fruit[7-i]
     
+# print(right_fruit)
+
 fruits = left_fruit[0:4] + right_fruit[0:4] + left_fruit[4:8] + right_fruit[4:8]
+
+# print(fruits)
 
 for i in range(16):
     for j in range(2):
@@ -23,10 +33,5 @@ for i in range(16):
 f = open("store_fruits_count.txt",'w')
 f.write("tree_number / normal_fruit / disease_fruit\n")
 for i in range(16):
-    f.write(str(i+1))
-    f.write(" / ")
-    f.write(fruits[i][0])
-    f.write(" / ")
-    f.write(fruits[i][1])
-    f.write("\n")
+    f.write(str(i+1)+" / "+fruits[i][0]+" / "+fruits[i][1]+"\n")
 f.close()
